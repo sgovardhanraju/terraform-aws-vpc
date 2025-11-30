c8194a1333c126df52885c7b04f36ff2ee43c1a2
Govardhan Raju <gsurapur@gmail.com>
2025-11-30 22:09:30 +0530

    session-38

--- vpc.tf
+++ vpc.tf
@@ -3,6 +3,7 @@ resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags = merge(
     var.vpc_tags,
